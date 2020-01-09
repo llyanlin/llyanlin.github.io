c8c3dbd77488635d067a6a267230f75764138ece
llyanlin <1819040332@qq.com>
2020-01-09 11:17:09 +0800

    换回来

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,34 @@
-/*
-	Halcyonic by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+function toggleMenu() {
+  var nav = document.getElementsByClassName("site-header-nav")[0];
+  if (nav.style.display == "inline-flex") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "inline-flex";
+  }
+}
 
-(function($) {
+jQuery(function() {
+  // 回到顶部
+  function toTop () {
+    var $toTop = $(".gotop");
 
-	var $window = $(window),
-		$body = $('body');
+    $(window).on("scroll", function () {
+      if ($(window).scrollTop() >= $(window).height()) {
+        $toTop.css("display", "block").fadeIn();
+      } else {
+        $toTop.fadeOut();
+      }
+    });
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
+    $toTop.on("click", function (evt) {
+      var $obj = $("body,html");
+      $obj.animate({
+        scrollTop: 0
+      }, 240);
 
-	// Nav.
+      evt.preventDefault();
+    });
+  }
 
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
-
-})(jQuery);
+  toTop();
+});
